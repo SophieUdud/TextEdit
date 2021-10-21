2141a11e0419b077b9a6c3e469ce75f5569e7c2d
Karina G <siriuscoder108@yahoo.com>
2021-10-20 21:05:07 -0700

    Added eventhandling for Edit and Cancel click

--- force-app/main/default/lwc/textEditor/textEditor.js
+++ force-app/main/default/lwc/textEditor/textEditor.js
@@ -1,3 +1,26 @@
 import { LightningElement } from 'lwc';
 
-export default class TextEditor extends LightningElement {}
+export default class TextEditor extends LightningElement {
+    viewMode = true;
+    editMode = false;
+
+    handleEditClick(){
+        this.viewMode = false;
+        this.editMode = true;
+        console.log('handleEditMode');
+        console.log('viewMode = ' + this.viewMode);
+        console.log('editMode = ' + this.editMode);
+    }
+
+    handleViewClick(){
+        this.viewMode = true;
+        this.editMode = false;
+        console.log('handleViewMode');
+        console.log('viewMode = ' + this.viewMode);
+        console.log('editMode = ' + this.editMode);
+    }
+
+    handleSaveClick(){
+        // call backend someday
+    }
+}
