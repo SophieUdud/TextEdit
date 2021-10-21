c57ab174de0534d6b7eedc9182c1a3c865a7842c
Karina G <siriuscoder108@yahoo.com>
2021-10-20 21:06:35 -0700

    Removed comments

--- force-app/main/default/lwc/textEditor/textEditor.js
+++ force-app/main/default/lwc/textEditor/textEditor.js
@@ -7,17 +7,11 @@ export default class TextEditor extends LightningElement {
     handleEditClick(){
         this.viewMode = false;
         this.editMode = true;
-        console.log('handleEditMode');
-        console.log('viewMode = ' + this.viewMode);
-        console.log('editMode = ' + this.editMode);
     }
 
     handleViewClick(){
         this.viewMode = true;
         this.editMode = false;
-        console.log('handleViewMode');
-        console.log('viewMode = ' + this.viewMode);
-        console.log('editMode = ' + this.editMode);
     }
 
     handleSaveClick(){
